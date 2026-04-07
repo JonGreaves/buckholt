d8a531830c09144195b820e5bdc8f86a17b684f6
Jon Greaves <jonathan.greaves@opengi.co.uk>
2026-04-07 14:53:14 +0100

    Included Bootstrap JS

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,6 @@
 import type { Preview } from '@storybook/angular';
 import { setCompodocJson } from '@storybook/addon-docs/angular';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import docJson from '../documentation.json';
 
 setCompodocJson(docJson);
